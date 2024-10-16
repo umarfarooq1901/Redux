cc24d0d700b434cbba04a9d3caa7d884f36b20e7
Umar farooq <103427953+umarfarooq1901@users.noreply.github.com>
2024-10-16 22:46:34 +0530

    first commit

---
+++ src/redux/reducers.js
@@ -0,0 +1,9 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+
+
+export const simpleReducer = createReducer(initialState, (builder)=>{
+        builder.addCase('navbarData', (state, action)=>{
+            
+        })
+})
